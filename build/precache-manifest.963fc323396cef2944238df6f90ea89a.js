ba92d061ff3ed762fce2e640f7bb121fc2c4ba35
Thomas Melucci <tmeluccideveloper@gmail.com>
2020-05-14 20:43:24 -0400

    added built copy

---
+++ build/precache-manifest.963fc323396cef2944238df6f90ea89a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8d3b07e2a837f106ae045b6e65bead",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "12ab9294d7100f4af7ac",
+    "url": "/monsters-rolodex/static/css/main.535edd0d.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12ab9294d7100f4af7ac",
+    "url": "/monsters-rolodex/static/js/main.d7faa4b2.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
